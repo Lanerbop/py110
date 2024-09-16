d8d0dc39b27c736ac1c73633540b041c1079ec27
Lane <lanedwofford@gmail.com>
2024-09-16 22:41:56 +0000

    added saved easy2.py

--- small_exercises/easy2.py
+++ small_exercises/easy2.py
@@ -276,11 +276,11 @@
 
 # LIST AVERAGE -> forgot about sum function
 
-def average(integers):
-    total = sum(integers)
-    return total // len(integers)
+# def average(integers):
+#     total = sum(integers)
+#     return total // len(integers)
 
-print(average([1, 5, 87, 45, 8, 8]) == 25)        # True
-print(average([9, 47, 23, 95, 16, 52]) == 40)     # True
-print(average([7]) == 7)                          # True
+# print(average([1, 5, 87, 45, 8, 8]) == 25)        # True
+# print(average([9, 47, 23, 95, 16, 52]) == 40)     # True
+# print(average([7]) == 7)                          # True
 
